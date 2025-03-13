e75a70bbaf5e04d87cb716fc32b33edc821a2a1d
y2k <kaykayyaw@gmail.com>
2025-03-13 09:48:59 +0000

    print elements in an array in reverse

--- src/reverse.c
+++ src/reverse.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
-
+#define N 10
 
 int main()
 {
+    int nums[N], *pnum;  //array nums, pointer pnum of to hold the memory address of each element in the array
 
+    printf("Enter %d numbers:\n", N);  
+    pnum = nums;
+    while (pnum < nums + N)     //while pnum is of a lower memory address, take input
+        scanf("%d", pnum++);  //store the input in pnum and increment the memory address later (postfix)
 
+    
+    printf("Array: ");
+    for (pnum = nums; pnum < nums + N; pnum++)
+    {
+        printf("%d ", *pnum);
+    }
+    printf("\n");
 
+    printf("In reverse order: ");
+    for (pnum = nums + N -1; pnum >= nums; pnum-- )
+    {
+        printf(" %d", *pnum);
+    }
+
+    printf("\n");
 
     return 0;
 }
